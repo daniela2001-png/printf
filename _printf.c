4eeb4a6f6524844f8fa12df38dffdb34d56723cb
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-16 15:50:39 +0000

    [ADD]: adding _putchar, _printf, and updated library.

---
+++ _printf.c
@@ -0,0 +1,102 @@
+#include "holberton.h"
+
+/**
+ * specifier - prints a module when one precedes it.
+ * @ap: is a list of arguments.
+ * Return: a string
+ */
+int specifier (va_list ap)
+{
+	char spec = va_arg(ap, char);
+
+	if (spec == '%')
+	{
+		_putchar(spec);
+	}
+	else
+	{
+		_putchar('%');
+	}
+	return (1);
+}
+
+/**
+ * character - Entry point
+ * @ap: is a list of character
+ * Return: a char
+ */
+
+int character(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
+
+/**
+ * strings - Entry point
+ * @ap: is a list of arguments
+ * Return: a string
+ */
+int strings(va_list ap)
+{
+	char *string = va_arg(ap, char *);
+	int i;
+	for (i = 0; string[i]; i++)
+	{
+		_putchar(string[i]);
+	}
+	return (sizeof(string));
+}
+
+/**
+ * strings - Entry point
+ * @ap: is a list of arguments
+ * Return: a string
+ */
+int ints(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
+/**
+ * _printf - Entry point
+ * @format: is a list of strings
+ * Return: list of numbers
+ */
+int _printf(const char *format, ...)
+{
+
+	va_list ap;
+
+	int i = 0;
+	int j = 0;
+
+	fmt array[] = {
+		{'%', specifier},
+		{'c', character},
+		{'s', strings},
+		{'i', ints},
+		{'d', ints},
+		{'\0', '\0'}
+	};
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (j < 2)
+			{
+				if (format[i + 1] == array[j].cmp)
+				{
+					array[j].f(ap);
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	return (5);
+}
