3212c5c62b6b96dfe7f616ba4d53d5ca89c4960e
ChristianCampos-55 <1566@holbertonschool.com>
2020-03-17 04:01:39 +0000

    [ADD]: library.

--- holberton.h
+++ holberton.h
@@ -1,16 +1,27 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdarg.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
-typedef struct formats
+/**
+ * struct options - creates he structure to call functions.
+ * @s: defines the parameter to call the function.
+ * @f: stores the names of functions to call.
+ */
+
+typedef struct options
 {
-	char cmp;
-	int(*f)(va_list);
-} fmt;
+	char s;
+
+	void (*f)(va_list, char *, unsigned int *);
+} opt;
 
 int _printf(const char *format, ...);
-int _putchar(char);
+void print_char(va_list data, char *buffer, unsigned int *size);
+void print_string(va_list data, char *buffer, unsigned int *size);
+void print_pctg(va_list data, char *buffer, unsigned int *size);
+
 #endif
